56ece9889d3cc728ef69a75dd5f234b047baedb3
Simon Rondelez <simon@skyscrapers.eu>
2019-03-21 13:55:47 +0100

    bump kinesis monitoring

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_kinesis_stream" "kinesis_stream" {
 }
 
 module "kinesis_stream_monitoring" {
-  source              = "github.com/skyscrapers/terraform-cloudwatch//kinesis?ref=1.0.0"
+  source              = "github.com/skyscrapers/terraform-cloudwatch//kinesis?ref=1.1.0"
   kinesis_stream_name = "${aws_kinesis_stream.kinesis_stream.name}"
   sns_topic_arn       = "${var.sns_topic_arn}"
 }
